1a5d2d0e2ec84c313a7cfbe66e62948260dca833
JPlante9117 <jacrplante@gmail.com>
2020-02-27 17:06:25 -0500

    Updates

---
+++ precache-manifest.0694c26313a20bda95816d37db7bea26.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e818a095a2dd431774edaaa98c3e5d4c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "a354d1ee92c3fb8cca4e",
+    "url": "/portfolio/static/css/2.5bc09813.chunk.css"
+  },
+  {
+    "revision": "383a07cea076e987c89b",
+    "url": "/portfolio/static/css/main.5b784035.chunk.css"
+  },
+  {
+    "revision": "a354d1ee92c3fb8cca4e",
+    "url": "/portfolio/static/js/2.5c43eaba.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/portfolio/static/js/2.5c43eaba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "383a07cea076e987c89b",
+    "url": "/portfolio/static/js/main.62916316.chunk.js"
+  },
+  {
+    "revision": "9aad3fec52ed5b5aae9b",
+    "url": "/portfolio/static/js/runtime-main.54c79d05.js"
+  },
+  {
+    "revision": "a59ba2f09c99326aba3209f92e1c86c6",
+    "url": "/portfolio/static/media/forummaker.a59ba2f0.png"
+  },
+  {
+    "revision": "c2655dcb1156b8e5b2248186f32bc9a4",
+    "url": "/portfolio/static/media/gameorganizer.c2655dcb.png"
+  },
+  {
+    "revision": "04359e185ebb4d65ea7a928b83098e05",
+    "url": "/portfolio/static/media/portfolio picture.04359e18.png"
+  },
+  {
+    "revision": "32bc5c4bf0a86fda7c56551076945be8",
+    "url": "/portfolio/static/media/researchlogger.32bc5c4b.png"
+  },
+  {
+    "revision": "640149d234d7a6c671b6fcb28c5744f2",
+    "url": "/portfolio/static/media/runenumbergame.640149d2.png"
+  },
+  {
+    "revision": "68cd624f5478e0766506335e71715561",
+    "url": "/portfolio/static/media/wcsulogo.68cd624f.png"
+  }
+]);
