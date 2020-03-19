2d77a95c7fc61fcb0779b951e4e923eb779fc880
JPlante9117 <jacrplante@gmail.com>
2020-03-19 16:32:35 -0400

    changed animationOnce on contact & offset=0 on top

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,19 +5,19 @@ const Contact = () => {
     return (
         <div className="footer">
             <div className="contact-container">
-                <ScrollAnimation animateIn="fadeInDown">
+                <ScrollAnimation animateIn="fadeInDown" animateOnce={true}>
                     <a id="contact"><h1>Let's Get In Touch</h1></a>
                 </ScrollAnimation>
-                <ScrollAnimation animateIn="fadeInRight" delay={1000}>
+                <ScrollAnimation animateIn="fadeInRight" delay={1000} animateOnce={true}>
                     <hr/>
                 </ScrollAnimation>
-                <ScrollAnimation animateIn="fadeInUp" delay={2000}>
+                <ScrollAnimation animateIn="fadeInUp" delay={2000} animateOnce={true}>
                     <div className="contact-text">
                         Interested in having me on your team, or just want to say hi?<br/><br/>
                         You can click the button below to get in touch!
                     </div>
                 </ScrollAnimation>
-                <ScrollAnimation animateIn="fadeInUp" delay={2500}>
+                <ScrollAnimation animateIn="fadeInUp" delay={2500} animateOnce={true}>
                     <a href="mailto:jacrplante@gmail.com" target="_blank" className="contactMe">Reach Out</a>
                 </ScrollAnimation>
             </div>
--- src/containers/HeaderContainer.js
+++ src/containers/HeaderContainer.js
@@ -23,7 +23,7 @@ export default class HeaderContainer extends Component {
                         </p>
                     </div>
                 </ScrollAnimation>
-                <ScrollAnimation animateIn="fadeIn" animateOnce={true} delay={3000}>
+                <ScrollAnimation animateIn="fadeIn" animateOnce={true} delay={3000} offset={0}>
                     <div className="row">
                         <SocialMediaContainer />
                     </div>
