a32c1c67fa260fb53f25f01abeda4f30d23573fe
JPlante9117 <jacrplante@gmail.com>
2020-03-19 15:23:49 -0400

    styled blog for mobile

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Blog = props => {
     return (
         <a href={props.link} className="blog">
-            <h2>{props.title}</h2>
+            <h2 className="blogTitle">{props.title}</h2>
             <p className="blogDate">Posted {props.posted}</p>
             <p className="blogDesc">{props.desc}</p>
         </a>
