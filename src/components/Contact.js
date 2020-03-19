853cfd5dd98bf6abcef4c7235a5c2f9d291f98d7
JPlante9117 <jacrplante@gmail.com>
2020-03-19 15:59:14 -0400

    styled contact for mobile and desktop

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,21 +4,23 @@ import ScrollAnimation from 'react-animate-on-scroll'
 const Contact = () => {
     return (
         <div className="footer">
-            <ScrollAnimation animateIn="fadeInDown">
-                <a id="contact"><h1>Let's Get In Touch</h1></a>
-            </ScrollAnimation>
-            <ScrollAnimation animateIn="fadeInRight" delay={1000}>
-                <hr/>
-            </ScrollAnimation>
-            <ScrollAnimation animateIn="fadeInUp" delay={2000}>
-                <div style={{margin: 10 + "px"}}>
-                    Interested in having me on your team, or just want to say hi?<br/>
-                    You can click the button below to get in touch!
-                </div>
-            </ScrollAnimation>
-            <ScrollAnimation animateIn="fadeInUp" delay={2500}>
-                <a href="mailto:jacrplante@gmail.com" target="_blank" className="contactMe">Reach Out</a>
-            </ScrollAnimation>
+            <div className="contact-container">
+                <ScrollAnimation animateIn="fadeInDown">
+                    <a id="contact"><h1>Let's Get In Touch</h1></a>
+                </ScrollAnimation>
+                <ScrollAnimation animateIn="fadeInRight" delay={1000}>
+                    <hr/>
+                </ScrollAnimation>
+                <ScrollAnimation animateIn="fadeInUp" delay={2000}>
+                    <div className="contact-text">
+                        Interested in having me on your team, or just want to say hi?<br/><br/>
+                        You can click the button below to get in touch!
+                    </div>
+                </ScrollAnimation>
+                <ScrollAnimation animateIn="fadeInUp" delay={2500}>
+                    <a href="mailto:jacrplante@gmail.com" target="_blank" className="contactMe">Reach Out</a>
+                </ScrollAnimation>
+            </div>
         </div>
     )
 }
